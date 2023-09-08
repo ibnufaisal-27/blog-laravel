029493734aa6f0d8018f4f7224dd1de152e3bec0
Ibnu Faisal <mibnufaisal@gmail.com>
2023-09-08 13:14:10 +0700

    added: seeder and factory

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,7 +5,7 @@
 <article>
     <h2>{{ $post->title }}</h2>
 
-    <p>By : Ibnu Faisal in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By : <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }} </a>in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
     {!! $post->body !!}
 </article>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,12 +2,16 @@
 
 @section('container')
 
+<h1 class="mb-5">Blog Posts</h1>
+
     @foreach ($posts as $post)
-    <article class="mb-5">
-        <h2>
-            <a href="/post/{{ $post->slug }}">{{ $post->title }}</a>
-        </h2>
-        {{ $post->excerpt }}
+    <article class="mb-5 border-bottom pb-3">
+        <h2> <a href="/post/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h2>
+        
+        <p>By : <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }} </a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+
+        <p>{{ $post->excerpt }}</p>
+        <a href="/post/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
     </article>
     @endforeach
 
